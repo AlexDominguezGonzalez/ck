a4b4ed1c1b15ae9b4d2782d0208b8c5072c05afc
AlexDominguezGonzalez <alexdodogo1809@gmail.com>
2026-09-16 20:02:19 +0200

    Actualización del proyecto

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <main className="flex min-h-svh items-center justify-center bg-purple-700 p-6">
-      <section className="flex h-72 w-72 items-center justify-center bg-red-600 p-8 text-center font-[Comic_Sans_MS,Comic_Sans,cursive] text-xl text-white">
+      <section className="flex min-h-svh w-96 items-center justify-center bg-red-600 p-8 text-center font-[Comic_Sans_MS,Comic_Sans,cursive] text-xl text-white">
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
           feugiat, justo at commodo posuere, risus libero porta nulla.
